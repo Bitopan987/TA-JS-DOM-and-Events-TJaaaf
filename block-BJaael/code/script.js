1198d7aa3d37a7821bd4c12f6b103e04dc303dd3
Bitopan Deka <bitopanxon1@gmail.com>
2021-04-18 07:34:54 +0530

    Refactor

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,16 +1,54 @@
-// function getRandomColor(elm) {
-//     var x = Math.floor(Math.random() * 256);
-//     var y = Math.floor(Math.random() * 256);
-//     var z = Math.floor(Math.random() * 256);
-//     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-//     elm.style.background = bgColor;
-//   }
+function getRandomNum(max) {
+  return Math.floor(Math.random() * max);
+}
 
-//   let first = document.querySelector(".first");
-//   let second = document.querySelector(".second");
-//   first.addEventListener("click", function () {
-//     getRandomColor(first);
-//   });
-//   second.addEventListener("mousemove", function () {
-//     getRandomColor(second);
-//   });
+function generateRandomColor() {
+  let hexCharacters = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = getRandomNum(16);
+    color = color + hexCharacters[randomNumber];
+  }
+  return color;
+}
+
+let parentBox = document.querySelector(".boxes");
+
+// create 500 boxes
+
+for (let i = 0; i < 600; i++) {
+  let div = document.createElement("div");
+  div.classList.add("box");
+  let h3 = document.createElement("h3");
+  let randomNo = getRandomNum(500);
+  h3.innerText = randomNo;
+  div.append(h3);
+  parentBox.append(div);
+}
+
+let allBoxes = document.querySelectorAll(".box");
+function handMouseMove() {
+  allBoxes.forEach((box) => {
+    box.style.backgroundColor = generateRandomColor();
+    box.children[0].innerText = getRandomNum(500);
+  });
+}
+
+parentBox.addEventListener("mousemove", handMouseMove);
