a985f26c2c448cdfc9e310a1432714f7d0fa0752
Bitopan Deka <bitopanxon1@gmail.com>
2021-04-22 02:27:30 +0530

    Refactor

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,3 +1,21 @@
-function ghzd9(event) {
-  console.log(event);
-}
+let firstBoxes = document.querySelectorAll(".first li");
+let secondBoxes = document.querySelectorAll(".second");
+
+firstBoxes.forEach((box, index) => {
+  box.addEventListener("click", (event) => {
+    event.target.innerText = index + 1;
+    setTimeout(() => {
+      event.target.innerText = " ";
+    }, 2000);
+  });
+});
+
+secondBoxes.forEach((box) => {
+  box.addEventListener("click", (event) => {
+    let text = event.target.dataset.text;
+    event.target.innerText = text;
+    setTimeout(() => {
+      event.target.innerText = " ";
+    }, 2000);
+  });
+});
