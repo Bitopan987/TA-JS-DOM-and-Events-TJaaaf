eed5251e749b9c6cad2c46adb545f1ef29894523
Bitopan Deka <bitopanxon1@gmail.com>
2021-04-25 01:25:23 +0530

    Completed Using forms with Events

---
+++ block-BJaaer/code/script.js
@@ -0,0 +1,16 @@
+let form = document.querySelector("form");
+let userInfo = {};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  userInfo.name = form.elements.name.value;
+  userInfo.email = form.elements.email.value;
+  userInfo.movies = form.elements.movies.value;
+  userInfo.color = form.elements.color.value;
+  userInfo.range = form.elements.range.value;
+  userInfo.drone = form.elements.drone.value;
+  userInfo.terms = form.elements.terms.checked;
+  console.log(userInfo);
+}
+
+form.addEventListener("submit", handleSubmit);
